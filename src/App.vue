1e0ba31e5bf81437c7d80337aff27e354a4facf1
protango <zacharymurray@hotmail.com>
2022-02-18 20:47:46 +1000

    Framework

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <header>
+    <div class="header-buffer">
+      <fa icon="circle-question" class="headerBtn" />
+    </div>
+    <h1 class="logo">Wordle Coach</h1>
+    <div class="header-buffer">
+      <fa icon="square-poll-vertical" class="headerBtn" />
+      <fa icon="gear" class="headerBtn" />
+    </div>
+  </header>
+  <main></main>
+  <footer>
+    <span
+      >Built with <i class="heart">❤</i> by
+      <a href="https://github.com/protango">protango</a></span
+    ><br />
+    <span>Not endorsed by, nor affiliated with the New York Times</span>
+  </footer>
 </template>
 
 <script lang="ts">
@@ -16,12 +32,75 @@ export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@font-face {
+  font-family: suez-one;
+  src: url("./assets/SuezOne-Regular.ttf");
+}
+
+header {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #d3d6db;
+  padding: 0 16px;
+  white-space: nowrap;
+}
+
+.header-buffer {
+  flex: 1 0 0;
+}
+
+.header-buffer:last-child {
+  text-align: right;
+}
+
+.logo {
+  font-family: suez-one;
+  font-size: 28px;
+  margin: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0 8px;
+  transform: scale(1, 1.25);
+}
+
+.headerBtn {
+  width: 21px;
+  height: 21px;
+  cursor: pointer;
+  padding: 0 4px;
+}
+
+footer {
+  font-size: 14px;
+  color: #777;
+  text-align: center;
+  line-height: 20px;
+  padding-bottom: 8px;
+}
+
+.heart {
+  color: #e25555;
+  font-style: normal;
+}
+
+@media (max-width: 400px) {
+  .logo {
+    font-size: 24px;
+  }
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+main {
+  flex: 1 1 auto;
+}
+
+body {
+  margin: 0;
+  font-family: sans-serif;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCircleQuestion,
+  faSquarePollVertical,
+  faGear,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+library.add(faCircleQuestion, faSquarePollVertical, faGear);
+
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
